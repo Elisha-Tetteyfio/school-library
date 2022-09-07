1e62ff75892ed68639d716929f7b1fe4483fcb0a
Elisha Tetteyfio <93135004+Elisha-Tetteyfio@users.noreply.github.com>
2022-09-07 12:54:35 +0000

    Add add_person method

--- app.rb
+++ app.rb
@@ -1,11 +1,14 @@
-require './book.rb'
+require './book'
+require './student'
+require './teacher'
 
 class App
   attr_accessor :books, :people
 
   def initialize
     @books = []
-    @people = []
+    @students = []
+    @teachers = []
   end
 
   def list_books
@@ -13,6 +16,35 @@ class App
   end
 
   def list_people
-    @people
+    return @students, @teachers
+  end
+
+  def create_person
+    p "Do you want to create a 1. Student  2. Teacher"
+    user_input = gets.chomp.to_i
+    
+    if user_input == 1
+      p "Enter age"
+      age = gets.chomp.to_i
+      p "Enter classroom"
+      classroom = gets.chomp
+      new_student = Student.new(age, classroom)
+      @students << new_student
+      puts "#{new_student} added successfully"
+    elsif user_input == 2
+      p "Enter age"
+      age = gets.chomp.to_i
+      p "Enter specialization"
+      specialization = gets.chomp
+      new_teacher = Teacher.new(age, specialization)
+      @teachers << new_teacher
+      puts "#{new_teacher} added successfully"
+    else 
+      puts "Wrong input option"
+      self.create_person
+    end
   end
 end
+
+pp = App.new
+pp.create_person
