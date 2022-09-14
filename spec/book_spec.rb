11d80efe017f3c2bafe5d29dbd433fbd4b63b18f
Elisha Tetteyfio <93135004+Elisha-Tetteyfio@users.noreply.github.com>
2022-09-14 15:03:29 +0000

    Test book Class

---
+++ spec/book_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe Book do
+  before :each do
+    @book = Book.new("Maths book", "Maths author")
+  end
+
+  it "Should be a book object" do
+    expect(@book).to be_an_instance_of(Book)
+  end
+  it "Should return the book's title" do
+    expect(@book.title).to eq("Maths book")
+  end
+  it "Should return the book's author" do
+    expect(@book.author).to eq("Maths author")
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,2 @@
 require './classroom'
+require './book'
