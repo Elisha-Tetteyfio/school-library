f8e9d61aa18a5833c13547c0191763dc9961e17a
Elisha Tetteyfio <93135004+Elisha-Tetteyfio@users.noreply.github.com>
2022-09-14 09:51:58 +0000

    Make required changes

--- interractions.rb
+++ interractions.rb
@@ -9,9 +9,11 @@ class Interraction
       app.list_people
       interraction(app)
     when 3
+      app.all_people
       CreatePerson.new(app.people).create_person
       interraction(app)
     when 4
+      app.all_books
       CreateBook.new(app.books).create_book
       interraction(app)
     when 5
